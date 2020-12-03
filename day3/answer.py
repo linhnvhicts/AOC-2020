9724b7aeffd0312d88a2f0c6d227c75642f84340
linhnvhicts <linhnvh@icts.vn>
2020-12-03 17:47:44 +0700

    update day3 answer

--- day3/answer.py
+++ day3/answer.py
@@ -1,36 +1,31 @@
-# import sys
-# sys.setrecursionlimit(10000)
-
 with open('input.txt') as f:
     content = f.readlines()
 content = [list(x.replace('\n', '')) for x in content] 
-height = len(content)
+total_height = len(content)
 width = len(content[0])
 
-def traverse(row, height):
-    tree_count = 0
-    if height < height + 1:
-        try:
-            next_down = content[height + 1][row + 3]
+def traverse(row, height, tree_count, shift_right, shift_down):
+    if height < total_height - 1:
+        current_index = row + shift_right
+        if current_index < width:
+            next_down = content[height + 1][current_index]
             if next_down == "#":
                 tree_count += 1
-            next_row = row + 3
-        except IndexError:
-            bad_row = row + 3
-            new_row = bad_row - width - 1 # start from index new_row of that line
-            next_down = content[height + 1][new_row + 3]
+            next_row = current_index
+        else:
+            current_index = row - width + shift_right
+            next_down = content[height + 1][current_index]
             if next_down == "#":
                 tree_count += 1
-            next_row = new_row + 3
+            next_row = current_index
         
-        next_heigth = height + 1
-        
-        traverse(next_row, next_heigth)
+        next_heigth = height + shift_down
+        traverse(next_row, next_heigth, tree_count, shift_right, shift_down)
     else:
-        return tree_count
+        print(tree_count)
 
 
-print(traverse(0, 0))
+traverse(0, 0, 0, 3, 1)
 
                 
 
